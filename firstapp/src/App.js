feb511f91f5ef722a5d690cf7314d7178f9a30bf
Jayaraja S.K <jayaraja19041@cse.ssn.edu.in>
2022-05-10 18:07:14 +0530

    Added React App for Login.

---
+++ firstapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
